51a1e28eb8f9e9a60e7f20c68cb118efc8136995
Eva Lai <evalai.dc@gmail.com>
2017-07-22 13:51:10 -0400

    added comments for functions

--- js/app.js
+++ js/app.js
@@ -37,47 +37,48 @@ function LunchIndexControllerFunction($http, $scope){
      console.log(err)
    });
 }
-
-function getSelected(){
-  var selected = $("#myselect").val()
-  getYearData(selected)
-}
-
-function getYearData(sel ){
-  console.log(sel)
-  $.ajax({
-    url:`https://data.maryland.gov/resource/7dst-j5if.json?school_year=${sel}`,
-    success: function(res){
-              self.year_data = res
-             console.log(self.year_data)
-           }
-  })
-  counters()
-
-}
-
-// code for the counters
-function counters() {
-$('.counter').each(function() {
-  var num = $(this),
-      countTo = num.attr('data-count');
-  $({ countNum: num.text()}).animate({
-    countNum: countTo
-  },
-
-  {
-
-    duration: 2300,
-    easing:'linear',
-    step: function() {
-      num.text(Math.floor(this.countNum));
-    },
-    complete: function() {
-      num.text(this.countNum);
-      //alert('finished');
+  // getting the value of the year selected
+    function getSelected(){
+      var selected = $("#myselect").val()
+      getYearData(selected)
     }
 
-  });
+  // Pulling in specific data according to year
+    function getYearData(sel ){
+      console.log(sel)
+      $.ajax({
+        url:`https://data.maryland.gov/resource/7dst-j5if.json?school_year=${sel}`,
+        success: function(res){
+                  self.year_data = res
+                 console.log(self.year_data)
+               }
+      })
+      counters()
 
-});
-}
+    }
+
+  // code for the counters
+    function counters() {
+    $('.counter').each(function() {
+      var num = $(this),
+          countTo = num.attr('data-count');
+      $({ countNum: num.text()}).animate({
+        countNum: countTo
+      },
+
+      {
+
+        duration: 2300,
+        easing:'linear',
+        step: function() {
+          num.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          num.text(this.countNum);
+          //alert('finished');
+        }
+
+      });
+
+    });
+    }
